c459e6335c6017c4c905efcfd329eade584882c9
6ent1 <gentitaga@outlook.com>
2024-07-20 05:58:06 -0400

    Configure Github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/se_project_react/",
   plugins: [react()],
   server: {
     port: 3000,
