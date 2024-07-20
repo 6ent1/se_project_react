722240b0c4345fc899de24c1d2c64453226bc259
6ent1 <gentitaga@outlook.com>
2024-07-20 05:32:00 -0400

    Upload Sprint 10 first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import avatar from "../../assets/Avatar.svg";
+import logo from "../../assets/Logo.svg";
+import "./Header.css";
+
+function Header({ handleAddClick, weatherData }) {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="WTWR logo" />
+      <p className="header__date-and-location">
+        {currentDate}, {weatherData.city}
+      </p>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        + Add clothes
+      </button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
